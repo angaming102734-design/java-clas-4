e97f0e98ec14121b0fa2ed563671c44a133de56a
ather ali <a.ngaming102734@gmail.com>
2025-11-25 16:42:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+//  var totalCost = 1 + 3 * 4;
+
+//  document.write (totalCost)
+
+//  var num1 = 1+(3*4);
+//   var num2 =(1+3) *4;
+  
+//   document.write(''Total coast ': num1); 
+//   document.write'Total ': num2');
+
+// document.write (num1)
+
+//  var text1 = ('the' + '<br>' );
+//  var text2 = ('world' + '<br>' );
+//   document.write(text1+text2)
+
+
+//  console.log(name);
+
+// var num1=  prompt('enter first num');
+// var num2= prompt('enter first num'); 
+
+// document.write(Number (num1) + Number (num2));
+
+var x = prompt("Where dose the popo live?")
+ if (x === "Pakistan") {
+    alert("Nice Atnswer")
+ };
+   
